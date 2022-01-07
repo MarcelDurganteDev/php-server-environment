acccf6739f84a6c81d883fb6023f2b31ecccf996
MarcelDurganteDev <info@marceldurgante.dev>
2022-01-07 03:01:16 +0100

    first commit

---
+++ close_session.php
@@ -0,0 +1,1 @@
+<!-- "close_session.php": This is the last file which will be responsible for destroying the session and redirecting to the login page. -->
---
+++ index.php
@@ -0,0 +1,24 @@
+<!-- "index.php" : this will be the login page with user and password fields and a login button. This fields must be in a form pointing its action to the validation script. In case you log in or are already logged in, you must be automatically redirected to the panel. -->
+<?php
+
+if (isset($_POST['username'])) {
+    $username = $_POST['username'];
+    echo $username;
+}
+
+?>
+
+<html>
+
+<head>
+    <title>Login App PHP</title>
+</head>
+
+<body>
+    <form method="post" action="index.php">
+        <input type="" name="username" placeholder="What is your name?">
+        <input type="submit" value="Save">
+    </form>
+</body>
+
+</html>
